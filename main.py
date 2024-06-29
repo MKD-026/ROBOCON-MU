ca6b480df39dd43455f2826591f53eb1a3f0c388
Mrinmoy Kumar Das <dasmrinmoy02@gmail.com>
2024-06-30 01:43:01 +0530

    Test for color logic using test images

---
+++ main.py
@@ -0,0 +1,68 @@
+print("loading")
+import cv2
+import numpy as np
+from ultralytics import YOLO
+import time
+from logic import *
+print("load complete")
+
+# load model
+model = YOLO("weights/yolov8n.pt", "v8")
+
+#for video input
+# cap = cv2.VideoCapture(0)
+# if not cap.isOpened():
+#     print("Cannot open camera")
+#     exit()
+
+prev_frame_time = 0
+new_frame_time = 0
+while True:
+    # ret, frame = cap.read()
+    frame = cv2.imread("data/test2.jpeg")
+    image_height, image_width = frame.shape[0], frame.shape[1]
+    imgcenter_x = image_width / 2
+    imgcenter_y = image_height / 2
+
+    new_frame_time = time.time()
+    # Predict on image
+    detect_params = model.predict(source=[frame], conf=0.2, save=False)
+
+    DP = detect_params[0].numpy()
+    # print(DP)
+
+    if len(DP) != 0:
+        for i in range(len(detect_params[0])):
+            boxes = detect_params[0].boxes
+            box = boxes[i]  # returns one box
+            clsID = box.cls.numpy()[0]
+            conf = box.conf.numpy()[0]
+            bb = box.xyxy.numpy()[0]
+
+            center_x = (bb[0] + bb[2]) / 2
+            center_y = (bb[1] + bb[3]) / 2
+            cv2.rectangle(frame, (int(bb[0]), int(bb[1])), (int(bb[2]), int(bb[3])), (0, 255, 0), 3)
+
+            # Extract bounding box coordinates
+            x1, y1, x2, y2 = bb
+            x = int(x1)
+            y = int(y1)
+            w = int(x2 - x1)
+            h = int(y2 - y1)
+
+            # Extract the region of interest (ROI) from the bounding box
+            roi = frame[y:y + h, x:x + w]
+
+            dominant_color = get_dominant_color(roi)
+            cv2.putText(frame, str(dominant_color), (int(x + 20), int(y)),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)  # black color
+
+    # Display the resulting frame
+    cv2.imshow("ObjectDetection", frame)
+
+    # Terminate run when "Q" pressed
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+
